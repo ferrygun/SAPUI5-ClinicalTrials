1b38b61406910b4f3bcf633d8133c0b9c5468564
Ferry Djaja <ferrygun@yahoo.com>
2018-08-28 12:01:15 +0800

    Update ct.js

--- ct.js
+++ ct.js
@@ -7,7 +7,7 @@ const request = require('request');
 const app = express();
 const fs = require('fs');
 
-let jsondata;
+let jsondata = [];
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -20,80 +20,65 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
 
 	let condition = req.query.condition;
+	let country = req.query.country;
 	let q = req.query.q;
 	let loc = req.query.loc;
+	let status = req.query.status;
+	let gender = req.query.gender;
+	let age = req.query.age;
+	let state = req.query.state;
+	let statecode = '';
+
+	
 
 	//console.log(condition + ' ' + q);
 
 	if (typeof q == 'undefined' && typeof loc == 'undefined') {
-		console.log('A');
-		//http://localhost/nodejs?condition=asthma
-		ClinicalTrials.search({condition: condition}).then(trials => {
+		//http://localhost/nodejs?condition=asthma&country=US
+
+
+		if(country == 'US') {
+			if(typeof state != 'undefined') {
+				if(state.length > 0) {
+					for(let i=0; i<jsondata.length; i++) {
+						//console.log(jsondata[i].state);
+						if(jsondata[i].state.toLowerCase() == state.toLowerCase()) {
+							statecode = jsondata[i].code;
+							console.log(statecode);
+							break;
+						}
+					}
+				}
+			}
+		}
+
+
+		ClinicalTrials.search({condition: condition, country, status, gender, age, statecode}).then(trials => {
 			//console.log(trials);
 			res.end(JSON.stringify(trials));
 		});
 	} else if (typeof q != 'undefined' && typeof loc == 'undefined') {
-		console.log('B');
 		//http://localhost/nodejs?q=NCT00001372
 		ClinicalTrials.searchD({condition: q}).then(trials => {
 			//console.log(trials);
 			res.end(JSON.stringify(trials));
 		});
 	}
-
-	if (typeof loc != 'undefined') {
-		console.log('C');
-		let len = jsondata.features.length;
-		let lat, lng = 0;
-
-		if(len > 0) {
-			for(var i=0; i<len; i++) {
-				if(loc.toLowerCase() === jsondata.features[i].properties.name.toLowerCase()) {
-					lat = jsondata.features[i].properties.latitude;
-					lng = jsondata.features[i].properties.longitude;
-
-					console.log(loc + ':' + lat + ':' + lng);
-					res.end(generateJson(lat, lng));
-					break;
-				}
-			}
-			res.end("no data");
-		} else 
-			res.end("no data");
-		
-	}
-
 })
 
-function generateJson(lat, lng) {
-	let json = "{";
-	json = json + "lat: " + lat + ",\n";
-	json = json + "lng: " + lng;
-	json = json + "}";
-	return json;
-}
-
-function sleep(milliseconds) {
-	var start = new Date().getTime();
-	for (var z = 0; z < 1e7; z++) {
-		if ((new Date().getTime() - start) > milliseconds){
-			break;
-		}
-	}
-}
 
 let port = process.env.PORT || 5000;
 
 function start() {
-	fs.readFile('data/country.geojson', 'utf8', function (err, data) {
+	fs.readFile("data/usstate.json", 'utf8', function (err, data) {
 		if (err) throw err;
 		else {
 			jsondata = JSON.parse(data);
-			
+
 			// Spin up the server
 			app.listen(app.get('port'), function() {
 				console.log('running on port', app.get('port'))
-			})
+			});
 		}
 	});
 }
